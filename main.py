c1a1d9357ccbcaab3dd2f3c215b443be01072a80
ssssssili <96482809+ssssssili@users.noreply.github.com>
2022-01-15 14:34:25 +0100

    Update main.py
    update network structure and convergence function.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, Dense
 
 
 def load_data(file_name, header_column):
@@ -40,6 +42,7 @@ headers, data = load_data("database_all_numerical_filtered.csv", 104)
 training, testing, = train_test_split(headers, data, 0.05, False)
 
 print(testing)
+'''
 model = models.Sequential()
 
 model.add(layers.Input(shape=training.shape[1:]))
@@ -51,6 +54,53 @@ model.compile(optimizer='adam',
                metrics=['accuracy'])
 
 model.fit(training, training)  # Second one determines output size of the model
+'''
+nodes=5  #nodes each output related to 
+length=training.shape[1:] 
+inputs = Input(shape=length)
+
+# define independent output
+class output_layer(Model):
+  def __init__(self, nodes):
+    super(output_layer, self).__init__()
+    self.layer1 = Dense(nodes, activation='tanh')
+    self.layer2 = Dense(1)
+  def call(self, x):
+    x = self.layer1(x)
+    return self.layer2(x)
+
+# creat output branches
+output = list()
+for i in range(length):
+  temp = output_layer(nodes)(inputs)
+  output.append(temp)
+
+model = Model(inputs=inputs, outputs=output)
+model.compile(optimizer='adam', loss=keras.losses.mean_squared_error) # temorary optimizer
+predict = model(train)
+
+# when reach the convergence begin parameter updating
+def convergence(train, training):
+    predict = model(train)
+    while True:
+        new_train = (train + predict) / 2 
+        if (train - new_train) < 0.1: # not sure about this condition
+            model.fit(train, training) # update parameters
+            break
+        else:
+            train = new_train
+            predict = model(train)
+
+# set the epoch of model, use number or a certain condition. before reach this epoch continue running the convergence function
+def model_epoch(train, training, somecondition):
+    while True:
+        if somecondition: # not sure about this condition
+            break
+        else:
+            convergence(train, training)
+
+model_epoch(training, training, somecondition)
+
 print(len(training))
 print(len(testing))
 print(model.predict(testing))
