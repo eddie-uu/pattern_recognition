7eb573e4dff8dca7f0c659ffbeb0910d4bdfec4d
Eddie <c.l.wong@students.uu.nl>
2022-01-21 10:21:25 +0100

    updated main file, refractor some lines

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow import keras
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Dense
 
 
 def load_data(file_name, header_column):
@@ -16,95 +17,123 @@ def load_data(file_name, header_column):
 def train_test_split(headers, data, test_size, random):
     split = int(len(data) * test_size)
 
-    # for checking the model with something more simplistic
-    # orbital_period_days_index = np.where(headers == "Orbital Period Days")[0][0]
-    # mass_index = np.where(headers == "Mass")[0][0]
-
     if random:
         np.random.shuffle(data)
 
     test_data = data[0: split]
     training_data = data[split: len(data)]
 
-    # x_train = training_data[:, orbital_period_days_index:mass_index + 1]
-    # x_test = test_data[:, orbital_period_days_index:mass_index + 1]
-
     x_train = np.asarray(test_data).astype('float32')
     x_test = np.asarray(training_data).astype('float32')
 
-    return x_train, x_test  # np.nan_to_num(x_train), np.nan_to_num(x_test)
+    return x_train, x_test
 
 
-headers, data = load_data("database_all_numerical_filtered.csv", 104)
-
+headers, data = load_data("databases/nasa_filtered.csv", 104)
 training, testing, = train_test_split(headers, data, 0.05, False)
 
-print(testing)
-model = models.Sequential()
 
-model.add(layers.Input(shape=training.shape[1:]))
-# If you get an error related to incorrect output size, change units to the same size as the shape of your second fit
-model.add(layers.Dense(units=67, activation="relu"))
+# randomly remove some data
+def random_remove(df, rate):
+    dataset = df.reset_index()
+    melt_one = pd.melt(dataset, id_vars = ['index'])
+    sampled = melt_one.sample(frac = rate).reset_index(drop = True)
+    dataset = sampled.pivot(index = 'index', columns = 'variable', values= 'value')
 
-model.compile(optimizer='adam',
-               loss='categorical_crossentropy',
-               metrics=['accuracy'])
-
-model.fit(training, training)  # Second one determines output size of the model
-print(len(training))
-print(len(testing))
-print(model.predict(testing))
-exit()
-
-# Below is an example setup, we can tweak this
-
-    # PREPROCESSING
-def filtering(full_dataset):    # Filter out all the rows that are missing data (and thus can't be used for training)
-    ...
-    return filtered_dataset
-
-def divide_dataset(complete_dataset):   #takes a (complete) dataset and splits it into two parts (one for training, one for validation)
-    ...
-    return training_set, validation_set
-
-def make_incomplete_set(complete_dataset):     # Remove some datapoints from a (complete) dataset to make a training dataset
-    ... 
-    return incomplete_dataset 
-
-def preprocessing(dataset):
-    complete_dataset = filtering(dataset)
-    complete_training_set, complete_validation_set = divide_dataset(complete_dataset)
-    incomplete_training_set = make_incomplete_set(complete_training_set)
-    incomplete_validation_set = make_incomplete_set(complete_validation_set)
-    return complete_training_set, incomplete_training_set, complete_validation_set, incomplete_validation_set
-
-    # MODEL
-def fill_in_blanks(incomplete_data): # Fill in some average values for missing data
-    ...
-    return estimated_data
-
-def is_complete(data):   #checks if there are values missing in the data, returning False if values are missing or True if the data is complete
-    ...
-    return True/False
-
-def not_converged(data1, data2):    # Checks if the difference between the two data vectors is smaller than some threshold (keep in mind to normalize values!)
-    ...
-    return True/False
-
-def apply_model(input_data):
-    if is_complete(input_data):
-        return input_data
-    else:
-        ...
-        old_data = fill_in_blanks(input_data)
-        new_data = old_data
-        while not_converged(old_data, new_data):
-            old_data = new_data
-            new_data = 0.5*(new_data + tf.ourmodel.applymodel(new_data))  # We'll obviously need to use different functions for this, this is more of an example of what it should do
-    return new_data 
-    # TRAINING
-# honestly, I'm not quite sure what the tf methods for training a model that is called inside a function is
+    return dataset
 
 
+# impute mean value into dataset
+def fill_mean(dataset):
+    for column in list(dataset.columns[dataset.isnull().sum() > 0]):
+        mean_val = dataset[column].mean()
+        dataset[column].fillna(mean_val, inplace=True)
+
+    return dataset
 
 
+# preprocess of dataset
+y_train = pd.DataFrame(training)
+y_test = pd.DataFrame(testing)
+
+x_train = random_remove(y_train, 0.5)
+x_test = random_remove(y_test, 0.5)
+
+fill_mean(y_train)
+fill_mean(x_train)
+fill_mean(y_test)
+fill_mean(x_test)
+# to test if all values are imputed: print (y_train.isnull().sum().sum())
+
+x_train = np.asarray(x_train).astype('float32')
+y_train = np.asarray(y_train).astype('float32')
+x_test = np.asarray(x_test).astype('float32')
+y_test = np.asarray(y_test).astype('float32')
+
+# parameters of model
+nodes = 5
+times = 5
+
+
+# model with expectation-maximization algorithm
+class EMAmodel(Model):
+    def __init__(self, x, nodes, times):
+        super(EMAmodel, self).__init__()
+        self.times = times
+        self.length = x.shape[1]
+        self.layer = []
+
+        for i in range(self.length):   # create branches
+            self.layer.append(tf.keras.Sequential(
+                layers=[Dense(nodes, activation='tanh', input_shape=(self.length,)),Dense(1)], name=None))
+
+    def call(self, x):
+        for i in range(self.times):   # iterations to calculate the mean value of x^n and x^(n+1)
+            tmp = []
+            for j in range(self.length):      # create multi-output network
+                tmp.append(self.layer[j](x))
+            output = tf.concat(tmp, 1)
+            x = (x+output)/2
+        return x
+
+
+model = EMAmodel(x_train, nodes, times)  # instantiate model
+loss_object = keras.losses.MeanSquaredError()
+optimizer = keras.optimizers.Adam(learning_rate=0.1)
+
+
+def evaluation(predictions, y):  # evaluate r2
+    sse = np.sum((y - predictions) ** 2)
+    sst = np.sum((y - np.mean(y)) ** 2)
+    return 1 - sse / sst
+
+
+def train(x_train, y_train):  # training step
+    with tf.GradientTape() as tape:
+        predictions = model(x_train)
+        loss = loss_object(y_train, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)            # calculate gradients
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))  # optimize parameters
+    r2 = evaluation(predictions, y_train)
+    return loss, r2
+
+
+epochs = 20000
+for epoch in range(epochs):  # training epochs
+    loss, r2 = train(x_train, y_train)
+    if (epoch % 100) == 0:
+        template = 'Epoch {}, Loss {}, Evaluation {}'
+        print(template.format(epoch, loss, r2))
+
+
+def test(x_test, y_test):  # testing step
+    predictions = model(x_test)
+    loss = loss_object(y_test, predictions)
+    r2 = evaluation(predictions, y_test)
+    template = 'Loss {}, Evaluation {}'
+    print(template.format(loss, r2))
+
+
+test(x_test, y_test)          # test
+
+# predict only: predictons = model(dataset)
